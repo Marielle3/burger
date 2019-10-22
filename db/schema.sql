89b999931f8430bc7179d5512744432d0e36cd8d
Marielle Ibarreta <ma.ibarreta3@gmail.com>
2019-10-22 09:44:26 -0700

    schema with data

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+DROP TABLE IF EXISTS burgers;
+
+CREATE TABLE burgers (
+  id INT AUTO_INCREMENT NOT NULL,
+  burger_name VARCHAR(255) NOT NULL,
+  devoured BOOLEAN NOT NULL DEFAULT false,
+  PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('cheeseburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('bacon cheeseburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('cowboy burger', FALSE);
